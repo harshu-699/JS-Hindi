9802c3d481c8dee1350e0fbc9c2c4d9f913d3839
harshu-699 <harshupatidar699@gmail.com>
2024-10-25 11:51:22 +0000

    datatypes

--- .devcontainer/01_basic/datatypes.js
+++ .devcontainer/01_basic/datatypes.js
@@ -9,3 +9,17 @@ console.log(3
 console.log("harsh") // right way
 
 
+let name="harsh"
+
+//number => 2 to power 53
+//bigint
+//String =>""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => uniqueness
+
+//object
+
+console.log(typeof "harsh")
+
